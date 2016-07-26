86bff3ae16bef1bde8a45efa3ddfb5e904a6cd7f
Jacob Tomlinson <jacob@tom.linson.uk>
2016-07-26 22:58:22 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+from opsdroid.skills import match_regex
+import logging
+import random
+
+def setup(opsdroid):
+    logging.debug("Loaded hello module")
+
+@match_regex(r'hi|hello')
+def hello(opsdroid, message):
+    response = random.choice(["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
+    message.respond(response)
+
+@match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
+def goodbye(opsdroid, message):
+    response = random.choice(["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
+    message.respond(response)
