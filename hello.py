9d3846ad818d6834e31ff61dbddfb01c7cefb492
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-11-10 16:28:16 +0000

    Added coroutines

--- hello.py
+++ hello.py
@@ -6,11 +6,11 @@ def setup(opsdroid):
     logging.debug("Loaded hello module")
 
 @match_regex(r'hi|hello|hey|hallo')
-def hello(opsdroid, message):
+async def hello(opsdroid, message):
     text = random.choice(["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
     await message.respond(text)
 
 @match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
-def goodbye(opsdroid, message):
+async def goodbye(opsdroid, message):
     text = random.choice(["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
     await message.respond(text)
